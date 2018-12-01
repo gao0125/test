f632f41a4a40d9bab8e5e07beba4495f13837867
yansongyuan123 <H5@gangmao.biz>
2018-12-01 15:09:20 +0800

    1201

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '115621607' );
+define( "WB_SKEY" , 'f9ffe13bfaf215a80bab9a9b4cd35b17' );
+define( "WB_CALLBACK_URL" , 'https://m.gangmao.biz/weibo_callback.php' );
